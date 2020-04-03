9d3a2002b4bdf71151142f273ee3b2ac5c911646
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-03 11:56:34 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -6,11 +6,12 @@ class Dancer
 
   include Dance
   extend MetaDancing
+  
 
-    attr_accessor :name
+  attr_accessor :name
 
-    def initialize(name)
-      @name = name
-    end
+  def initialize(name)
+    @name = name
+  end
 
 end
